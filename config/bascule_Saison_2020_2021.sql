e3a52f9a60333246f2cc7eb05b18ce97abe8d168
Thierry SUTTER <t.sutter@applicam.com>
2020-07-18 21:32:18 +0200

    nouveautés 2020

--- config/bascule_Saison_2020_2021.sql
+++ config/bascule_Saison_2020_2021.sql
@@ -29,3 +29,31 @@ INSERT INTO `competition` (`id`, `LIBELLE`, `TYPE_COMPETITION`, `CATEGORIE`, `DI
 
 -- on passe toutes les rencontres de la saison passée au statut "effectué"
 update rencontre set statut = 1, auteur_maj='TSUTTER' where competition < 152 and statut = 0 ;
+
+create table groupe 
+(
+    ID int(11) not null,
+    LIBELLE varchar(100) not null,
+    COMPETITION int(11) not null,
+    EQUIPE varchar(100) not null,
+    AUTEUR varchar(25) NOT NULL,
+    DERNIERE_MAJ timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `groupe`
+  ADD PRIMARY KEY (`ID`);
+
+insert into groupe (ID, LIBELLE, COMPETITION, EQUIPE, AUTEUR, DERNIERE_MAJ) values 
+(1, 'Régionale 2 Groupe D', 152, 'RS AMANVILLERS', 'TSUTTER', CURRENT_TIMESTAMP),
+(2, 'Régionale 2 Groupe D', 152, 'CS BLENOD', 'TSUTTER', CURRENT_TIMESTAMP),
+(3, 'Régionale 2 Groupe D', 152, 'ES GANDRANGE', 'TSUTTER', CURRENT_TIMESTAMP),
+(4, 'Régionale 2 Groupe D', 152, 'RS MAGNY', 'TSUTTER', CURRENT_TIMESTAMP),
+(5, 'Régionale 2 Groupe D', 152, 'APM METZ 2', 'TSUTTER', CURRENT_TIMESTAMP),
+(6, 'Régionale 2 Groupe D', 152, 'METZ ESAP', 'TSUTTER', CURRENT_TIMESTAMP),
+(7, 'Régionale 2 Groupe D', 152, 'ASP THIONVILLE', 'TSUTTER', CURRENT_TIMESTAMP),
+(8, 'Régionale 2 Groupe D', 152, 'AS MONTIGNY LES METZ', 'TSUTTER', CURRENT_TIMESTAMP),
+(9, 'Régionale 2 Groupe D', 152, 'FC PONT A MOUSSON', 'TSUTTER', CURRENT_TIMESTAMP),
+(10, 'Régionale 2 Groupe D', 152, 'AS ST JULIEN LES METZ', 'TSUTTER', CURRENT_TIMESTAMP),
+(11, 'Régionale 2 Groupe D', 152, 'USAG UCKANGE', 'TSUTTER', CURRENT_TIMESTAMP),
+(12, 'Régionale 2 Groupe D', 152, 'FC YUTZ', 'TSUTTER', CURRENT_TIMESTAMP)
+;
